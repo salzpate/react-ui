0b4e5d4f04093872723762989066245b5e5b78c9
Ronny Krammer <ronny.krammer@gwvs.de>
2021-04-19 23:26:17 +0200

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+# @salzpate React UI
+
+React UI component library built with Tailwind CSS.
+
+## Getting Started
+
+Run for development:
+
+```bash
+npm run storybook
+# or
+yarn storybook
+```
+
+Open [http://localhost:3000](http://localhost:6006) with your browser to see the result.
+
+## Reference
+
+- [tailwindcss](https://tailwindcss.com)
+- [Heroicons](https://heroicons.com)
+- [TypeScript](https://www.typescriptlang.org)
+
+## License
+Ronny Krammer is Open Source software released under the https://www.apache.org/licenses/LICENSE-2.0.html[Apache 2.0 license].
---
+++ components/PageSection/PageSection.stories.tsx
@@ -0,0 +1,30 @@
+import React, { PropsWithChildren } from "react";
+import { Meta, Story } from '@storybook/react';
+import PageSection, { PageSectionProps } from "./PageSection";
+
+export default {
+  title: 'Components/PageSection',
+  component: PageSection,
+} as Meta;
+
+const Template: Story<PropsWithChildren<PageSectionProps>> = (args) => <PageSection {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  id: 'id1',
+  headline: 'Headline 1',
+};
+
+export const WithSubSection = Template.bind({});
+WithSubSection.args = {
+  id: 'id2',
+  headline: 'Headline 2',
+  subSection: true
+};
+
+export const WithClassName = Template.bind({});
+WithClassName.args = {
+  id: 'id3',
+  headline: 'Headline 3',
+  className: 'bg-blue-500'
+};
