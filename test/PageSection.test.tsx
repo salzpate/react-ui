212b0e7cb28324b1d79134b0c3bf3c31e69503ef
Ronny Krammer <ronny.krammer@gwvs.de>
2022-07-14 19:54:34 +0200

    fix test

--- test/PageSection.test.tsx
+++ test/PageSection.test.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
+import { createRoot } from 'react-dom/client';
 import * as ReactDOM from 'react-dom';
 import { PageSection } from '../src/';
 
 describe('Thing', () => {
-  it('renders without crashing', () => {
-    const div = document.createElement('div');
-    ReactDOM.render(<PageSection headline="Headline" id="headline1" />, div);
-    ReactDOM.unmountComponentAtNode(div);
+  test('renders without crashing', () => {
+    const container = document.createElement('div');
+    const root = createRoot(container!);
+    root.render(<PageSection headline="Headline" id="headline1" />);
+    root.unmount();
   });
 });
