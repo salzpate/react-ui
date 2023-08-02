9f2cb47819c2471a90c600fd598ba36c4b23c6d0
Ronny Krammer <ronny.krammer@gwvs.de>
2023-08-02 10:54:07 +0200

    remove snackbar provider

--- src/components/SnackBar/index.ts
+++ src/components/SnackBar/index.ts
@@ -1,4 +1,2 @@
-export { default as SnackBarProvider, useSnackBar } from './SnackBarProvider';
-export type { SnackBarProviderProps, SnackBarContextType } from './SnackBarProvider';
 export { default as SnackBar, SnackBarTypes } from './SnackBar';
 export type { SnackBarProps } from './SnackBar';
