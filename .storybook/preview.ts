d35f1101e920499c831eac4d19a42ec24fcc4905
Ronny Krammer <ronny.krammer@gwvs.de>
2025-02-17 23:49:26 +0100

    Darkmode in storybook

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,3 +1,5 @@
+import { withThemeByDataAttribute } from '@storybook/addon-themes';
+
 import '../styles/globals.css';
 
 export const parameters = {
@@ -9,3 +11,14 @@ export const parameters = {
     },
   },
 }
+
+export const decorators = [
+  withThemeByDataAttribute({
+    themes: {
+      light: 'light',
+      dark: 'dark',
+    },
+    defaultTheme: 'light',
+    attributeName: 'data-mode',
+  }),
+];
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from 'tailwindcss';
 
 export default {
-  content: ['./src/components/**/*.tsx']
+  content: ['./src/components/**/*.tsx'],
 } satisfies Config;
