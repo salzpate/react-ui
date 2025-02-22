7cc507708f1002ec2faca2139db673bd6faab647
Ronny Krammer <ronny.krammer@gwvs.de>
2025-02-22 18:29:15 +0100

    fix pagelink dark

--- src/components/PageLink/PageLink.tsx
+++ src/components/PageLink/PageLink.tsx
@@ -6,7 +6,7 @@ function PageLink(props: Readonly<DetailedHTMLProps<AnchorHTMLAttributes<HTMLAnc
   const { children, className, ...rest } = props;
 
   return (
-    <a className={cn('text-link hover:underline hover:decoration-dotted', className)} {...rest}>
+    <a className={cn('text-link hover:underline hover:decoration-dotted dark:text-link-dark dark:hover:text-link-dark-hover', className)} {...rest}>
       {children}
     </a>
   );
