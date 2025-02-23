9fb8a1f7dfafbf3a538ae87658b1e68c2e9000d6
Ronny Krammer <ronny.krammer@gwvs.de>
2025-02-23 23:20:39 +0100

    fix cursor

--- src/components/MoreLink/MoreLink.tsx
+++ src/components/MoreLink/MoreLink.tsx
@@ -23,13 +23,11 @@ function MoreLink(props: Readonly<PropsWithChildren<MoreLinkProps>>): JSX.Elemen
       {headline && <h3 className={cn('text-lg tracking-tight text-gray-900 uppercase sm:text-xl lg:text-2xl lg:font-medium dark:text-gray-200', { 'mt-4': imgSrc })}>{headline}</h3>}
       <div className="my-4 text-sm font-light lg:text-base lg:font-normal">{children}</div>
       <div className="my-4">
-        <Link href={href} legacyBehavior>
-          <a className="inline-flex items-center gap-0 text-link hover:gap-1 hover:underline hover:decoration-dotted dark:text-link-dark dark:hover:text-link-dark-hover">
-            <span>Weitere Informationen</span>
-            <svg className="ml-1 h-3 w-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-              <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
-            </svg>
-          </a>
+        <Link href={href} className="inline-flex items-center gap-0 text-link hover:gap-1 hover:underline hover:decoration-dotted dark:text-link-dark dark:hover:text-link-dark-hover">
+          <span>Weitere Informationen</span>
+          <svg className="ml-1 h-3 w-3" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+            <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M9 5l7 7-7 7" />
+          </svg>
         </Link>
       </div>
     </div>
